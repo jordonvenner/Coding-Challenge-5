6f751416a4aae85caae3fc44b12daa46dddfe52f
Jordon Venner <jordonvenner@usf.edu>
2025-02-07 15:03:06 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1: Object Properties
+
+const customer = {
+    name: "Lane Berry",
+    age: 23,
+    email: "laneb@gmail.com"
+};
+
+console.log('Name: ${customer.name}');
+console.log('Age: ${customer.age}');
+console.log('Email: ${customer.email}');
