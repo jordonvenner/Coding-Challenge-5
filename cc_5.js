7a8c1947e26f3b53ff364567dd08d82663ccb385
Jordon Venner <jordonvenner@usf.edu>
2025-02-07 15:19:16 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,28 @@ const customer = {
 console.log('Name: ${customer.name}');
 console.log('Age: ${customer.age}');
 console.log('Email: ${customer.email}');
+
+
+//Task 2: Object Methods
+
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing"
+};
+
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    displayOrder: function() {
+        console.log('Order ID: ${this.orderID}');
+        console.log('Total Amount: $${this.totalAmount}');
+        console.log('Status: ${this.status}');
+    }
+};
+
+order.displayOrder();
+
+
